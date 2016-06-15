874a22d0d72b1305c6129d3978d907a35c420d97
kenpcjr <kennethcooke@me.com>
2016-06-15 16:56:15 -0400

    Framed text works

--- ios-collections-and-strings-problems/FISAppDelegate.m
+++ ios-collections-and-strings-problems/FISAppDelegate.m
@@ -6,13 +6,30 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    
+    NSString *lineToPrint = @"I think this works";
+    NSString *frameTopAndBottom = @"";
+    NSString *formattedLine = @"";
+    NSMutableString *wordBlock = [@"" mutableCopy];
+    NSArray *wordsToPrintArray = [lineToPrint componentsSeparatedByString:@" "];
+    NSUInteger frameWidth = [[wordsToPrintArray valueForKeyPath:@"@max.length"]unsignedIntegerValue] + 1;
+    
+    for (NSUInteger i = 0; i <= frameWidth + 2; i++) {
+        frameTopAndBottom = [frameTopAndBottom stringByAppendingString:@"*"];
+    }
+    for (NSUInteger i = 0; i < wordsToPrintArray.count; i++) {
+        NSString *wordToPad = wordsToPrintArray[i];
+        wordToPad = [wordToPad stringByPaddingToLength:frameWidth withString:@" " startingAtIndex:0];
+        
+        formattedLine = [NSString stringWithFormat:@"* %@*\n",wordToPad];
+        [wordBlock appendString:formattedLine];
+    }
+    NSString *frameWithText = [NSString stringWithFormat:@"\n%@\n%@%@",frameTopAndBottom, wordBlock, frameTopAndBottom];
+    
+    NSLog(@"%@",frameWithText);
+    
     return YES;
 }
-
-/**
- 
- * Write your method implementations here!
- 
- */
 
 @end
